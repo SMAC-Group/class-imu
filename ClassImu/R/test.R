57da297ba2a3d584e8c43802bde9fe444b1a5a5d
Stéphane Guerrier <stephaneguerrier@Stephanes-MacBook-Pro.local>
2018-01-15 18:30:54 +0100

    Initial commit

---
+++ ClassImu/R/test.R
@@ -0,0 +1,14 @@
+#' @title This funciton adds up two numbers
+#'
+#' @description What the functionkjh is doing
+#' @param x A \code{numeric} value.
+#' @param y A \code{numeric} value.
+#' @return A \code{numeric} which adds \code{x} and \code{y}.
+#' @author Stephane Guerrier
+#' @export
+#' @examples
+#' add(3,4)
+#' add(0,0)
+add = function(x, y){
+  x + y
+}
